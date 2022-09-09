1d6ed6740ac786ec8c202456461779c8c1fdabcb
AminaBrown <amina@datacult.com>
2022-09-09 13:47:48 -0400

    map and study structure updates

--- map.js
+++ map.js
@@ -25,15 +25,15 @@ let map = ((data, data2, data_map = {org_name:'Organization.Name', city:'City', 
     } else {
        // margins for SVG
     var margin = {
-        left: 40,
-        right: 40,
-        top: 180,
-        bottom: 250
+        left: 10,
+        right: 10,
+        top: 10,
+        bottom: 10
     } 
 
     // responsive width & height
     var svgWidth = 400//parseInt(d3.select(selector).style('width'), 10)
-    var svgHeight = svgWidth/0.65//(svgWidth*1.4)
+    var svgHeight = svgWidth/0.8//(svgWidth*1.4)
     }
     
 
@@ -60,13 +60,30 @@ let map = ((data, data2, data_map = {org_name:'Organization.Name', city:'City', 
 
     const partnerColorScale = d3.scaleOrdinal()
         .domain(["Youth Center","Science Advisor","Community Partner"])
-        .range(["#B1F1E2","#FFE599","#FBC28D"])
+        .range(["#52E0BE","#FFD966","#FAB269"])
 
-    // Map and projection
-    var scl = 0.35, mult = 2
-    const projection = d3.geoMercator()
-        .scale(width / scl / Math.PI)
-        .translate([width*mult+150, height*mult-150]);
+        if (window.outerWidth > 900){
+    
+            // Map and projection
+            var scl = 0.35, mult = 2
+            var projection = d3.geoMercator()
+            .scale(width / scl / Math.PI)
+            .translate([width*mult+150, height*mult-150]);
+
+            // variables
+            var circ_rad = 5, ak_trans = 'translate(200 600)', hi_trans = 'translate(1070 -570)', hi_scl = 2, icon_size = 30;
+
+            } else {
+             
+            // Map and projection
+            var scl = 0.35, mult = 1.55
+            var projection = d3.geoMercator()
+            .scale(width / scl / Math.PI)
+            .translate([width*mult+185, height*mult-300]);
+
+            // variables
+            var circ_rad = 2.5, ak_trans = 'translate(175 360)', hi_trans = 'translate(570 -300)', hi_scl = 2, icon_size = 25;
+        }
 
     // Load external data and boot
 d3.json("https://raw.githubusercontent.com/loganpowell/census-geojson/master/GeoJSON/500k/2021/state.json").then( function(geodata) {
@@ -79,7 +96,7 @@ d3.json("https://raw.githubusercontent.com/loganpowell/census-geojson/master/Geo
             .join("path")
                 .attr('class','country')
                 .attr('id',d => d.properties.NAME.replace(" ", "-"))
-                .attr("fill", "#F3F3F8")
+                .attr("fill", "#F0F0F5")
                 .attr("d", d3.geoPath()
                 .projection(projection)
                 )
@@ -87,8 +104,8 @@ d3.json("https://raw.githubusercontent.com/loganpowell/census-geojson/master/Geo
                 .style('stroke-width',1.25)
                 .style('opacity',.8);
 
-        d3.select("#Alaska").attr('transform',"translate(200 600) scale(0.5)")
-        d3.select("#Hawaii").attr('transform',"translate(1070 -570) scale(2)")
+        d3.select("#Alaska").attr('transform',ak_trans+" scale("+ak_scl+")")
+        d3.select("#Hawaii").attr('transform',hi_trans+" scale("+hi_scl+")")
         d3.select("#Puerto-Rico").attr('display',"none")
 
         document.getElementById('map-group').insertBefore(document.getElementById('states'), document.getElementById('circles'));
@@ -96,50 +113,73 @@ d3.json("https://raw.githubusercontent.com/loganpowell/census-geojson/master/Geo
     })
 
     var circ_group = svg.append('g').attr('id','circles')
+    var youth = circ_group.append('g').attr('id','youth')
+    var science = circ_group.append('g').attr('id','science')
+    var community = circ_group.append('g').attr('id','community')
 
     data.forEach(loc => {
 
         var proj = projection([loc[data_map.long],loc[data_map.lat]])
 
+        var group
+        (loc[data_map.type] == 'Youth Center') ? group = youth : (loc[data_map.type] == 'Science Advisor') ? group = science : group = community
+
         // Add the path using this helper function
-        circ_group
+        group
         .append('circle')
+        .attr('class',loc[data_map.state]+'circ')
+        .attr('id',loc[data_map.type].replace(' ','-'))
         .attr('cx', proj[0])
         .attr('cy', proj[1])
-        .attr('r', 5)
-        // .attr('opacity',.8)
+        .attr('r', circ_rad)
+        .attr('opacity',(loc[data_map.type] == 'Youth Center') ? .5 : 1)
         .attr('fill', partnerColorScale(loc[data_map.type]));
         
     });
+
+    // document.getElementById('circles').insertBefore(document.getElementById('Science-Advisor'), document.getElementById('Youth-Center'));
+    // document.getElementById('circles').insertBefore(document.getElementById('Community-Partner'), document.getElementById('Science-Advisor'));
 
     var icon_group = svg.append('g').attr('id','icons')
 
     data2.forEach(loc => {
 
         var proj = projection([loc[data_map.long],loc[data_map.lat]])
-        var popup_group = icon_group.append('g').attr('class','popups').attr('id',loc[data_map.name]).attr('display','none')
-
+        
         // Add the path using this helper function
         icon_group
         .append('image')
-        .attr('href','assets/icon.svg')
+        .attr('href','assets/Testimonial.svg')
+        .attr('class',loc[data_map.state])
+        .attr('id','testimonial'+loc[data_map.state])
         .attr('x', proj[0])
         .attr('y', proj[1])
-        .attr('height',20)
-        // .attr('opacity',.8);
+        .attr('height',icon_size)
+        .attr('width',icon_size*.863)
+        .attr('cursor','pointer')
         .on("click", function() {
 				popup_group.attr('display',1)
 			});
 
+        var icon_loc = document.getElementById('testimonial'+loc[data_map.state]).getBBox();
+        
+        d3.select('#testimonial'+loc[data_map.state])
+            .attr('x',proj[0]-icon_loc.width/4)
+            .attr('y',proj[1]-icon_loc.height/1.3);
+
+        var popup_group = icon_group.append('g').attr('class','popups').attr('id',loc[data_map.name]).attr('display','none')
+        var rect_width = 500, rect_height = 200
+
         popup_group
         .append('rect')
-        .attr('x',200)
-        .attr('y',100)
-        .attr('height',200)
-        .attr('width',500)
+        .attr('x',width/2-rect_width/2)
+        .attr('y',height/2-rect_height/2)
+        .attr('height',rect_height)
+        .attr('width',rect_width)
         .attr('fill','#F4F4FF')
         .attr('rx',50)
-        .attr('stroke','black')
+        .attr('stroke','white')
+        .attr('stroke-width',5)
 
         popup_group
         .append('text')
@@ -149,16 +189,22 @@ d3.json("https://raw.githubusercontent.com/loganpowell/census-geojson/master/Geo
         .attr('text-anchor','middle')
 
         popup_group
-        .append('text')
-        .attr('x',670)
-        .attr('y',140)
-        .text('X')
-        .attr('text-anchor','middle')
+        .append('image')
+        .attr('href','assets/x.svg')
+        .attr('x',width/2+rect_width/2-40)
+        .attr('y',height/2-rect_height/2+25)
+        .attr('height',15)
+        .attr('cursor','pointer')
         .on("click", function() {
 				popup_group.attr('display','none')
 			});
 
         
     });
+
+    var ak_scl = 0.5;
+    d3.selectAll('.AKcirc').attr('transform',ak_trans+" scale("+ak_scl+")").attr('r',circ_rad/ak_scl)
+    d3.selectAll('.HIcirc').attr('transform',hi_trans+" scale("+hi_scl+")").attr('r',circ_rad/hi_scl)
+
 
 })
---
+++ study.js
@@ -0,0 +1,347 @@
+'use strict'
+
+let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'Min', max:'Max'}, selector = '#study-placeholder') => {
+
+    ////////////////////////////////////
+    //////////// svg setup /////////////
+    ////////////////////////////////////
+
+    var body = d3.select(selector)
+    body.html("")
+
+    if (window.outerWidth > 900){
+    
+    // margins for SVG
+    var margin = {
+        left: 210,
+        right: 210,
+        top: 100,
+        bottom: 100
+    }
+
+    // responsive width & height
+    var svgWidth = 1400//parseInt(d3.select(selector).style('width'), 10)
+    var svgHeight = (svgWidth / 2)
+    } else {
+       // margins for SVG
+    var margin = {
+        left: 40,
+        right: 40,
+        top: 180,
+        bottom: 250
+    } 
+
+    // responsive width & height
+    var svgWidth = 400//parseInt(d3.select(selector).style('width'), 10)
+    var svgHeight = svgWidth/0.65//(svgWidth*1.4)
+    }
+    
+
+    // helper calculated variables for inner width & height
+    const height = svgHeight - margin.top - margin.bottom
+    const width = svgWidth - margin.left - margin.right
+
+    // add SVG
+    d3.select(".study-svg").remove();
+
+    const svg = body.append('svg')
+        // .attr('height', svgHeight)
+        // .attr('width', svgWidth)
+        .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
+        .attr('class', 'study-svg')
+        .append('g')
+        .attr('id','study-group')
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    ////////////////////////////////////
+    //////////scroll observers//////////
+    ////////////////////////////////////
+    let stp = 1;
+    let options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: [.75]
+      };
+        
+        const cope = document.querySelector('#cope');
+
+        const copeObserver = new IntersectionObserver(handleCope, options);
+    
+        function handleCope(entry, observer) {
+            if (entry[0].intersectionRatio > .75) {
+                stp = 1
+                update()
+            }
+        };
+    
+        copeObserver.observe(cope);
+
+        const use = document.querySelector('#use');
+
+        const useObserver = new IntersectionObserver(handleUse, options);
+    
+        function handleUse(entry, observer) {
+            if (entry[0].intersectionRatio > .75) {
+                stp = 2
+                update()
+            }
+        };
+
+        useObserver.observe(use);
+
+        const race = document.querySelector('#race');
+
+        const raceObserver = new IntersectionObserver(handleRace, options);
+    
+        function handleRace(entry, observer) {
+            if (entry[0].intersectionRatio > .75) {
+                stp = 3
+                update()
+            }
+        };
+
+        raceObserver.observe(race);
+
+        const gender = document.querySelector('#gender');
+
+        const genderObserver = new IntersectionObserver(handleGender, options);
+    
+        function handleGender(entry, observer) {
+            if (entry[0].intersectionRatio > .75) {
+                stp = 4
+                update()
+            }
+        };
+
+        genderObserver.observe(gender);
+
+
+    ////////////////////////////////////
+    //////////////scales////////////////
+    ////////////////////////////////////
+
+    // const partnerColorScale = d3.scaleOrdinal()
+    //     .domain(["Youth Center","Science Advisor","Community Partner"])
+    //     .range(["#B1F1E2","#FFE599","#FBC28D"])
+
+    //shapes
+    // data.forEach(topic => {
+
+        // const topicScale = d3.scaleLinear()
+        //                 .domain([topic[data_map.min],topic[data_map.max]])
+        //                 .range([0,200])
+
+        svg.append('path')
+            .attr('id','left')
+            .attr('d','M149.896 3.89856C107.555 -3.1948 69.1736 -3.02933 36.9835 25.3325C5.82038 52.7895 -2.64193 68.6471 0.671441 110.01C4.71535 160.493 40.6229 183.203 88.6686 199.451C139.048 216.488 185.554 215.608 221.46 176.429C256.266 138.45 262.115 91.8259 239.748 45.4436C221.081 6.73254 192.336 11.0086 149.896 3.89856Z')
+            // .attr('x',width/3)
+            // .attr('y',200)
+            // .attr('r',topicScale(topic[data_map.left]))
+            .attr('fill','purple')
+
+        svg.append('path')
+            .attr('id','right')
+            .attr('d','M149.896 3.89856C107.555 -3.1948 69.1736 -3.02933 36.9835 25.3325C5.82038 52.7895 -2.64193 68.6471 0.671441 110.01C4.71535 160.493 40.6229 183.203 88.6686 199.451C139.048 216.488 185.554 215.608 221.46 176.429C256.266 138.45 262.115 91.8259 239.748 45.4436C221.081 6.73254 192.336 11.0086 149.896 3.89856Z')
+            // .attr('x',width*2/3)
+            // .attr('y',200)
+            // .attr('transform','scale('+topicScale(topic[data_map.right])+')')
+            .attr('fill','gray')
+        
+    // });
+
+    //text
+    var font_size = 20, x_text = width/2, line_height = '4%';
+
+    let study1 =  svg.append('text')
+        .attr('class','step1')
+        .attr('text-anchor','middle')
+        .attr('x',x_text)
+        .attr('y',50)
+        .attr('font-size',font_size)
+        .style('opacity',0)
+
+    study1
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .text('The treatment group saw a significant')
+
+    study1
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .attr('dy',line_height)
+        .text('increase in certain coping skills')
+
+    let study2 =  svg.append('text')
+        .attr('class','step2')
+        .attr('text-anchor','middle')
+        .attr('x',x_text)
+        .attr('y',50)
+        .attr('font-size',font_size)
+        .style('opacity',0)
+
+    study2
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .text('The median time spent on the site')
+
+    study2
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .attr('dy',line_height)
+        .text('by imi users was 4.7 times longer')
+    
+    study2
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .attr('dy',line_height)
+        .text('than the resource list users')
+
+    let study3 =  svg.append('text')
+        .attr('class','step3')
+        .attr('text-anchor','middle')
+        .attr('x',x_text)
+        .attr('y',50)
+        .attr('font-size',font_size)
+        .style('opacity',0)
+
+    study3
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .text('78% of participants identified as')
+
+    study3
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .attr('dy',line_height)
+        .text('racial or ethnic minorities')
+
+    let study4 =  svg.append('text')
+        .attr('class','step4')
+        .attr('text-anchor','middle')
+        .attr('x',x_text)
+        .attr('y',50)
+        .attr('font-size',font_size)
+        .style('opacity',0)
+
+    study4
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .text('60% of participants identified as transgender, ')
+
+    study4
+        .append('tspan')
+        .attr('class','tspan')
+        .attr('x',x_text)
+        .attr('dy',line_height)
+        .text('gender non-conforming, or gender non-binary')
+
+    //scroll update function
+    function update(val){
+
+        // if (val) step = val.target.value;
+
+        if (stp == 1) {
+
+            var topic = data[0]
+
+            const topicScale = d3.scaleLinear()
+                        .domain([topic[data_map.min],topic[data_map.max]])
+                        .range([0,1.5])
+
+            
+            var left_loc = document.getElementById('left').getBBox();
+            var right_loc = document.getElementById('right').getBBox();
+            var left_scl = topicScale(topic[data_map.left]);
+            var right_scl = topicScale(topic[data_map.right]);
+
+            svg.selectAll('#left').attr('transform','translate('+(width/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
+            svg.selectAll('#right').attr('transform','translate('+(width*2/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
+
+            study1.style('opacity',1)
+            study2.style('opacity',0)
+            study3.style('opacity',0)
+            study4.style('opacity',0)
+
+            // svg.selectAll('.Coping').style('opacity',0)
+
+        } else if (stp == 2){
+
+            var topic = data[1]
+
+            const topicScale = d3.scaleLinear()
+                        .domain([topic[data_map.min],topic[data_map.max]])
+                        .range([0,1])
+
+            var left_loc = document.getElementById('left').getBBox();
+            var right_loc = document.getElementById('right').getBBox();
+            var left_scl = topicScale(topic[data_map.left]);
+            var right_scl = topicScale(topic[data_map.right]);
+
+            svg.selectAll('#left').attr('transform','translate('+(width/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
+            svg.selectAll('#right').attr('transform','translate('+(width*2/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
+            
+            
+            study1.style('opacity',0)
+            study2.style('opacity',1)
+            study3.style('opacity',0)
+            study4.style('opacity',0)
+
+        } else if (stp == 3){
+
+            var topic = data[2]
+
+            const topicScale = d3.scaleLinear()
+                        .domain([topic[data_map.min],topic[data_map.max]])
+                        .range([0,1])
+
+            var left_loc = document.getElementById('left').getBBox();
+            var right_loc = document.getElementById('right').getBBox();
+            var left_scl = topicScale(topic[data_map.left]);
+            var right_scl = topicScale(topic[data_map.right]);
+
+            svg.selectAll('#left').attr('transform','translate('+(width/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
+            svg.selectAll('#right').attr('transform','translate('+(width*2/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
+            
+            study1.style('opacity',0)
+            study2.style('opacity',0)
+            study3.style('opacity',1)
+            study4.style('opacity',0)
+
+        } else {
+
+            var topic = data[3]
+
+            const topicScale = d3.scaleLinear()
+                        .domain([topic[data_map.min],topic[data_map.max]])
+                        .range([0,1])
+
+            var left_loc = document.getElementById('left').getBBox();
+            var right_loc = document.getElementById('right').getBBox();
+            var left_scl = topicScale(topic[data_map.left]);
+            var right_scl = topicScale(topic[data_map.right]);
+
+            svg.selectAll('#left').attr('transform','translate('+(width/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
+            svg.selectAll('#right').attr('transform','translate('+(width*2/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
+            
+            study1.style('opacity',0)
+            study2.style('opacity',0)
+            study3.style('opacity',0)
+            study4.style('opacity',1)
+        }
+
+        
+
+    }
+    
+
+    
+})
