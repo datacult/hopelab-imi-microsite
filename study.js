de8d7a8126dc8e3c5df3d05adc5df9ca2456b363
AminaBrown <amina@datacult.com>
2022-09-22 13:21:54 -0400

    clean study code

--- study.js
+++ study.js
@@ -22,7 +22,7 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
     var scl_range = [0,1], line_height = '8%'
 
     // responsive width & height
-    var svgWidth = 600//parseInt(d3.select(selector).style('width'), 10)
+    var svgWidth = 600
     var svgHeight = (svgWidth / 2)
     } else {
        // margins for SVG
@@ -36,7 +36,7 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
     var scl_range = [0,.65], line_height = '4%'
 
     // responsive width & height
-    var svgWidth = 400//parseInt(d3.select(selector).style('width'), 10)
+    var svgWidth = 400
     var svgHeight = svgWidth/0.65//(svgWidth*1.4)
     }
     
@@ -49,8 +49,6 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
     d3.select(".study-svg").remove();
 
     const svg = body.append('svg')
-        // .attr('height', svgHeight)
-        // .attr('width', svgWidth)
         .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
         .attr('class', 'study-svg')
         .append('g')
@@ -122,53 +120,25 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
 
 
     ////////////////////////////////////
-    //////////////scales////////////////
+    ////////////////draw////////////////
     ////////////////////////////////////
 
-    // const partnerColorScale = d3.scaleOrdinal()
-    //     .domain(["Youth Center","Science Advisor","Community Partner"])
-    //     .range(["#B1F1E2","#FFE599","#FBC28D"])
-
-    //shapes
-    // data.forEach(topic => {
-
-        // const topicScale = d3.scaleLinear()
-        //                 .domain([topic[data_map.min],topic[data_map.max]])
-        //                 .range([0,200])
-
         svg.append('path')
+            .attr('class','shape')
             .attr('id','left')
             .attr('d','M149.896 3.89856C107.555 -3.1948 69.1736 -3.02933 36.9835 25.3325C5.82038 52.7895 -2.64193 68.6471 0.671441 110.01C4.71535 160.493 40.6229 183.203 88.6686 199.451C139.048 216.488 185.554 215.608 221.46 176.429C256.266 138.45 262.115 91.8259 239.748 45.4436C221.081 6.73254 192.336 11.0086 149.896 3.89856Z')
-            // .attr('x',width/3)
-            // .attr('y',200)
-            // .attr('r',topicScale(topic[data_map.left]))
             .attr('fill','#7638FB')
 
         svg.append('path')
+            .attr('class','shape')
             .attr('id','right')
             .attr('d','M149.896 3.89856C107.555 -3.1948 69.1736 -3.02933 36.9835 25.3325C5.82038 52.7895 -2.64193 68.6471 0.671441 110.01C4.71535 160.493 40.6229 183.203 88.6686 199.451C139.048 216.488 185.554 215.608 221.46 176.429C256.266 138.45 262.115 91.8259 239.748 45.4436C221.081 6.73254 192.336 11.0086 149.896 3.89856Z')
-            // .attr('x',width*2/3)
-            // .attr('y',200)
-            // .attr('transform','scale('+topicScale(topic[data_map.right])+')')
             .attr('fill','#D9D9D9')
 
-        var topic = data[0]
+        const topicScale = d3.scaleLinear();
+        var left_loc, right_loc, left_scl, right_scl;
 
-        const topicScale = d3.scaleLinear()
-                    .domain([topic[data_map.min],topic[data_map.max]])
-                    .range([0,1])
-
-        
-        var left_loc = document.getElementById('left').getBBox();
-        var right_loc = document.getElementById('right').getBBox();
-        var left_scl = topicScale(topic[data_map.left]);
-        var right_scl = topicScale(topic[data_map.right]);
-
-        svg.selectAll('#left').attr('transform','translate('+(width*2/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
-        svg.selectAll('#right').attr('transform','translate('+(width/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
-
-        
-    // });
+        update_shape(data[0],'#7638FB')
 
     //text
     var font_size = 20, x_text = width/2;
@@ -213,7 +183,7 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
         .text(' in certain coping skills')
         .attr('font-weight',400)
 
-    var hover_size = document.getElementById('hover-star').getBBox(), padding = 0;
+    var hover_size = document.getElementById('hover-star').getBBox();
 
     let hover_text =  svg.append('text')
         .attr('class','sig-hover')
@@ -259,7 +229,6 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
             .attr('display','none');
 
         document.getElementById('study-group').insertBefore(document.getElementById('sig-rect'), document.getElementById('sig-text'));
-    
         
         hover_text
         .attr('display','none')
@@ -366,10 +335,10 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
     var hover_rect = svg.append('rect')
         .attr('class','p-rect')
         .attr('id','hover-rect')
-        .attr('x',hover_size.x-padding)
-        .attr('y',hover_size.y-padding)
-        .attr('height',hover_size.height+padding*2)
-        .attr('width',hover_size.width+padding*2)
+        .attr('x',hover_size.x)
+        .attr('y',hover_size.y)
+        .attr('height',hover_size.height)
+        .attr('width',hover_size.width)
         .attr('opacity',0)
         .attr('display',1)
         .on("mouseover", function() {
@@ -378,6 +347,26 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
             d3.selectAll('.sig-hover').attr('display','none')
         });
 
+    function update_shape(topic, color){
+
+            topicScale
+                .domain([topic[data_map.min],topic[data_map.max]])
+                .range(scl_range)
+
+            left_loc = document.getElementById('left').getBBox();
+            right_loc = document.getElementById('right').getBBox();
+            left_scl = topicScale(topic[data_map.left]);
+            right_scl = topicScale(topic[data_map.right]);
+
+            svg.selectAll('#left')
+                .attr('transform','translate('+(width*2/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
+                .attr('fill',color)
+
+            svg.selectAll('#right')
+                .attr('transform','translate('+(width/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
+                .attr('fill','#D9D9D9')
+    }
+
     //scroll update function
     function update(val){
 
@@ -385,23 +374,7 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
 
         if (stp == 1) {
 
-            topic = data[0]
-
-            topicScale
-                .domain([topic[data_map.min],topic[data_map.max]])
-                .range(scl_range)
-                // .range([0,10])
-
-            
-            left_loc = document.getElementById('left').getBBox();
-            right_loc = document.getElementById('right').getBBox();
-            left_scl = topicScale(topic[data_map.left]);
-            right_scl = topicScale(topic[data_map.right]);
-
-            svg.selectAll('#left').attr('transform','translate('+(width*2/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
-                .attr('fill','#7638FB')
-            svg.selectAll('#right').attr('transform','translate('+(width/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
-                .attr('fill','#D9D9D9')
+            update_shape(data[0],'#7638FB')
 
             study1.style('opacity',1)
             hover_rect.attr('display',1)
@@ -409,25 +382,9 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
             study3.style('opacity',0)
             study4.style('opacity',0)
 
-            // svg.selectAll('.Coping').style('opacity',0)
-
         } else if (stp == 2){
 
-            topic = data[1]
-
-            topicScale
-                .domain([topic[data_map.min],topic[data_map.max]])
-                .range(scl_range)
-
-            left_loc = document.getElementById('left').getBBox();
-            right_loc = document.getElementById('right').getBBox();
-            left_scl = topicScale(topic[data_map.left]);
-            right_scl = topicScale(topic[data_map.right]);
-
-            svg.selectAll('#left').attr('transform','translate('+(width*2/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
-                .attr('fill','#7638FB')
-            svg.selectAll('#right').attr('transform','translate('+(width/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
-                .attr('fill','#D9D9D9')
+            update_shape(data[1],'#7638FB')
             
             study1.style('opacity',0)
             hover_rect.attr('display',"none")
@@ -437,21 +394,7 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
 
         } else if (stp == 3){
 
-            topic = data[2]
-
-            topicScale
-                .domain([topic[data_map.min],topic[data_map.max]])
-                .range(scl_range)
-
-            left_loc = document.getElementById('left').getBBox();
-            right_loc = document.getElementById('right').getBBox();
-            left_scl = topicScale(topic[data_map.left]);
-            right_scl = topicScale(topic[data_map.right]);
-
-            svg.selectAll('#left').attr('transform','translate('+(width*2/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
-                .attr('fill','#52E0BE')
-            svg.selectAll('#right').attr('transform','translate('+(width/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
-                .attr('fill','#D9D9D9')
+            update_shape(data[2],'#52E0BE')
 
             study1.style('opacity',0)
             hover_rect.attr('display',"none")
@@ -461,21 +404,7 @@ let study = ((data, data_map = {topic:'Topic', left:'Left', right:'Right', min:'
 
         } else {
 
-            topic = data[3]
-
-            topicScale
-                .domain([topic[data_map.min],topic[data_map.max]])
-                .range(scl_range)
-
-            left_loc = document.getElementById('left').getBBox();
-            right_loc = document.getElementById('right').getBBox();
-            left_scl = topicScale(topic[data_map.left]);
-            right_scl = topicScale(topic[data_map.right]);
-
-            svg.selectAll('#left').attr('transform','translate('+(width*2/3-left_loc.width*left_scl/2)+' '+(200-left_loc.height*left_scl/2)+') scale('+left_scl+')')
-                .attr('fill','#52E0BE')
-            svg.selectAll('#right').attr('transform','translate('+(width/3-right_loc.width*right_scl/2)+' '+(200-right_loc.height*right_scl/2)+') scale('+right_scl+')')
-                .attr('fill','#D9D9D9')
+            update_shape(data[3],'#52E0BE')
 
             study1.style('opacity',0)
             hover_rect.attr('display',"none")
